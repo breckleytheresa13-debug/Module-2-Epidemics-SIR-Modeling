0054c04a6901db51dd1a6be8121d8dc9fdb803ed
Theresa Breckley <breckleytheresa13@gmail.com>
2026-02-24 13:41:53 -0500

    Update exploratory_analysis_day1.py

--- Code/exploratory_analysis_day1.py
+++ Code/exploratory_analysis_day1.py
@@ -15,3 +15,12 @@ plt.ylabel('Active Infections')
 plt.title('Day vs Active Infections')
 plt.grid(True)
 plt.show()
+
+# approximaetely exponential growth, but not exactly. We can see that the number of active infections is increasing over time, but the rate of increase is not constant. It appears to be accelerating, which is characteristic of exponential growth, but there are some fluctuations in the data that suggest it may not be a perfect exponential curve.
+
+
+# Day 2: Estimate R_0 for the mystrey virus data using the fit for exponential growth in I.
+# Plot estimated exponential curve over your scatterplot from last class.
+
+# What viruses ahve a similar R_0?
+# How accurate do you this R_o estuiamte is?
